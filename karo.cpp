43b033038eb6df55b3547c292b1d337bb351b430
Nitin Bisht <3498georgian@gmail.com>
2017-09-19 00:30:17 +0530

    Add files via upload

---
+++ karo.cpp
@@ -0,0 +1,185 @@
+#include <sstream>
+#include <string>
+#include <iostream>
+#include <opencv2\opencv.hpp>
+
+using namespace cv;
+using namespace std;
+
+// these values are for the Trackbar (Hue , Saturation , Values)
+int H_MIN = 0;
+int H_MAX = 256;
+int S_MIN = 0;
+int S_MAX = 256;
+int V_MIN = 0;
+int V_MAX = 256;
+//default capture width and height
+const int FRAME_WIDTH = 640;
+const int FRAME_HEIGHT = 480;
+//max number of objects to be detected in frame
+const int MAX_NUM_OBJECTS = 50;
+//minimum and maximum object area
+const int MIN_OBJECT_AREA = 20 * 20;
+const int MAX_OBJECT_AREA = FRAME_HEIGHT*FRAME_WIDTH / 1.5;
+//names that will appear at the top of each window
+const string windowName = "Original Image";
+const string windowName1 = "HSV Image";
+const string windowName2 = "Thresholded Image";
+const string windowName3 = "After Morphological Operations";
+const string trackbarWindowName = "Trackbars";
+void on_trackbar(int, void*)
+{//This function gets called whenever a
+ // trackbar position is changed
+
+}
+string intToString(int number) {
+	stringstream ss;
+	ss << number;
+	return ss.str();
+}
+void createTrackbars() {
+	//create window for trackbars
+
+
+	namedWindow(trackbarWindowName, 0);
+	//create memory to store trackbar name on window
+	char TrackbarName[50];
+	sprintf_s(TrackbarName, "H_MIN", H_MIN);
+	sprintf_s(TrackbarName, "H_MAX", H_MAX);
+	sprintf_s(TrackbarName, "S_MIN", S_MIN);
+	sprintf_s(TrackbarName, "S_MAX", S_MAX);
+	sprintf_s(TrackbarName, "V_MIN", V_MIN);
+	sprintf_s(TrackbarName, "V_MAX", V_MAX);
+	//create trackbars and insert them into window
+	//3 parameters are: the address of the variable that is changing when the trackbar is moved(eg.H_LOW),
+	//the max value the trackbar can move (eg. H_HIGH), 
+	//and the function that is called whenever the trackbar is moved(eg. on_trackbar)
+	//                                  ---->    ---->     ---->      
+	createTrackbar("H_MIN", trackbarWindowName, &H_MIN, H_MAX, on_trackbar);
+	createTrackbar("H_MAX", trackbarWindowName, &H_MAX, H_MAX, on_trackbar);
+	createTrackbar("S_MIN", trackbarWindowName, &S_MIN, S_MAX, on_trackbar);
+	createTrackbar("S_MAX", trackbarWindowName, &S_MAX, S_MAX, on_trackbar);
+	createTrackbar("V_MIN", trackbarWindowName, &V_MIN, V_MAX, on_trackbar);
+	createTrackbar("V_MAX", trackbarWindowName, &V_MAX, V_MAX, on_trackbar);
+
+
+}
+void drawObject(int x, int y, Mat &frame) {
+
+	circle(frame, Point(x, y), 20, Scalar(0, 255, 0), 2);
+	if (y - 25>0)
+		line(frame, Point(x, y), Point(x, y - 25), Scalar(0, 255, 0), 2);
+	else line(frame, Point(x, y), Point(x, 0), Scalar(0, 255, 0), 2);
+	if (y + 25<FRAME_HEIGHT)
+		line(frame, Point(x, y), Point(x, y + 25), Scalar(0, 255, 0), 2);
+	else line(frame, Point(x, y), Point(x, FRAME_HEIGHT), Scalar(0, 255, 0), 2);
+	if (x - 25>0)
+		line(frame, Point(x, y), Point(x - 25, y), Scalar(0, 255, 0), 2);
+	else line(frame, Point(x, y), Point(0, y), Scalar(0, 255, 0), 2);
+	if (x + 25<FRAME_WIDTH)
+		line(frame, Point(x, y), Point(x + 25, y), Scalar(0, 255, 0), 2);
+	else line(frame, Point(x, y), Point(FRAME_WIDTH, y), Scalar(0, 255, 0), 2);
+
+	putText(frame, intToString(x) + "," + intToString(y), Point(x, y + 30), 1, 1, Scalar(0, 255, 0), 2);
+
+}
+void morphOps(Mat &thresh) {
+
+	Mat erodeElement = getStructuringElement(MORPH_RECT, Size(3, 3)); // kernel is 3x3 rectangle
+	//dilate with larger element so make sure object is nicely visible
+	Mat dilateElement = getStructuringElement(MORPH_RECT, Size(8, 8));
+
+	erode(thresh, thresh, erodeElement);
+	erode(thresh, thresh, erodeElement);
+
+	dilate(thresh, thresh, dilateElement);
+	dilate(thresh, thresh, dilateElement);
+
+}
+
+void trackFilteredObject(int &x, int &y, Mat threshold, Mat &cameraFeed) {
+
+	Mat temp;
+	threshold.copyTo(temp);
+
+	vector< vector<Point> > contours; // for output of contours
+	vector<Vec4i> hierarchy;
+	findContours(temp, contours, hierarchy, CV_RETR_CCOMP, CV_CHAIN_APPROX_SIMPLE);//to find the contours
+	//use moments method to find our filtered object
+	double refArea = 0;
+	bool objectFound = false;
+	if (hierarchy.size() > 0) {
+		int numObjects = hierarchy.size();
+		//if number of objects greater than MAX_NUM_OBJECTS we have a noisy filter
+		if (numObjects<MAX_NUM_OBJECTS) {
+			for (int index = 0; index >= 0; index = hierarchy[index][0]) {
+
+				Moments moment = moments((cv::Mat)contours[index]);
+				double area = moment.m00;
+
+				//if the area is less than 20 px by 20px then it is probably just noise
+				//if the area is the same as the 3/2 of the image size, probably just a bad filter
+				//we only want the object with the largest area so we safe a reference area each
+				//iteration and compare it to the area in the next iteration.
+				if (area>MIN_OBJECT_AREA && area<MAX_OBJECT_AREA && area>refArea) {
+					x = moment.m10 / area;
+					y = moment.m01 / area;
+					objectFound = true;
+					refArea = area;
+				}
+				else objectFound = false;
+
+
+			}
+			//let user know you found an object
+			if (objectFound == true) {
+				putText(cameraFeed, "Tracking Object", Point(0, 50), 2, 1, Scalar(0, 255, 0), 2);
+				//draw object location on screen
+				drawObject(x, y, cameraFeed);
+			}
+
+		}
+		else putText(cameraFeed, "TOO MUCH NOISE! ADJUST FILTER", Point(0, 50), 1, 2, Scalar(0, 0, 255), 2);
+	}
+}
+int main(int argc, char* argv[])
+{
+	bool trackObjects = true;//Make it true for detecting object
+	bool useMorphOps = false; // Change it into true for dilate and erode morphological operation 
+	Mat cameraFeed; // to store frame of webcam
+	Mat HSV; // for HSV image
+	Mat threshold; // Bianry Threshold Image
+	int x = 0, y = 0; // for location
+	createTrackbars(); // Trackbars
+	VideoCapture capture;
+	capture.open(0); // my webam at 0 location
+	
+	capture.set(CV_CAP_PROP_FRAME_WIDTH, FRAME_WIDTH);
+	capture.set(CV_CAP_PROP_FRAME_HEIGHT, FRAME_HEIGHT);
+	
+	
+	while (1) {
+		capture.read(cameraFeed);
+		
+		cvtColor(cameraFeed, HSV, COLOR_BGR2HSV);//convert from BGR to HSV
+		
+		inRange(HSV, Scalar(H_MIN, S_MIN, V_MIN), Scalar(H_MAX, S_MAX, V_MAX), threshold);//Filter image acc to threshold values
+		
+		//perform morphological operations on thresholded image to eliminate noise
+		if (useMorphOps)
+			morphOps(threshold);
+
+		if (trackObjects)
+			trackFilteredObject(x, y, threshold, cameraFeed);
+
+		imshow(windowName2, threshold);
+		imshow(windowName, cameraFeed);
+		imshow(windowName1, HSV);
+		
+		int k=waitKey(30);
+		if (k == 27)
+			return 0;
+	}
+
+	return 0;
+}
